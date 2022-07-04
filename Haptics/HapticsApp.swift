73be286310cee548de8cfe483549f3b2589640f8
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-04 11:15:16 +0200

    Initial Commit

---
+++ Haptics/HapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticsApp.swift
+//  Haptics
+//
+//  Created by Skorobogatow, Christian on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct HapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
